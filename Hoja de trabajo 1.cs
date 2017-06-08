3a6c290b784f9ce9d2c4889306f63aec020a559e
willop <wilfredp159@gmail.com>
2017-06-08 15:56:38 -0600

    Segunda Funcion
    Se modifico el archivo
    
    Funcion Vocales

--- Hoja de trabajo 1.cs
+++ Hoja de trabajo 1.cs
@@ -17,6 +17,7 @@ namespace Practica1IPC2
             InitializeComponent();
         }
 
+        //Hipotenusa
         private void button1_Click(object sender, EventArgs e)
         {
             int a = Convert.ToInt32(textBox1.Text);
@@ -27,5 +28,84 @@ namespace Practica1IPC2
             double hipotenusa = Math.Sqrt(suma);
             MessageBox.Show("la hipotenusa es: " + hipotenusa);
         }
+
+        //Vocales
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string texto = textBox1.Text;
+            Boolean letraa = false;
+            Boolean letrae = false;
+            Boolean letrai = false;
+            Boolean letrao = false;
+            Boolean letrau = false;
+
+            for (int i = 0; i < texto.Length; i++)
+            {
+                if (texto[i].Equals((char)97) || texto[i].Equals((char)65) || texto[i].Equals((char)160))
+                {
+                    letraa = true;
+                }
+                if (texto[i].Equals((char)101) || texto[i].Equals((char)69) || texto[i].Equals((char)130))
+                {
+                    letrae = true;
+                }
+                if (texto[i].Equals((char)105) || texto[i].Equals((char)73) || texto[i].Equals((char)161))
+                {
+                    letrai = true;
+                }
+                if (texto[i].Equals((char)111) || texto[i].Equals((char)79) || texto[i].Equals((char)162))
+                {
+                    letrao = true;
+                }
+                if (texto[i].Equals((char)117) || texto[i].Equals((char)85) || texto[i].Equals((char)163))
+                {
+                    letrau = true;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            if (letraa == true)
+            {
+                if (letrae == true)
+                {
+                    if (letrai == true)
+                    {
+                        if (letrao == true)
+                        {
+                            if (letrau == true)
+                            {
+                                MessageBox.Show("La plabra " + texto + " SI CUMPLE");
+                            }
+                            else
+                            {
+                                MessageBox.Show("La plabra " + texto + " NO CUMPLE, falta la vocal u");
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("La plabra " + texto + " NO CUMPLE, falta la vocal o");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("La plabra " + texto + " NO CUMPLE, falta la vocal i");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("La plabra " + texto + " NO CUMPLE, falta la vocal e");
+                }
+            }
+            else
+            {
+                MessageBox.Show("La plabra " + texto + " NO CUMPLE, falta la vocal a");
+            }
+
+        }
+
+
     }
 }
